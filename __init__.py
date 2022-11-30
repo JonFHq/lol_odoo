dd753737d1a8f697ec8f892a09b26d8d27414e4f
JonFHq <fernandez.hujon21@zaragoza.salesianos.edu>
2022-11-30 11:13:46 +0100

    Commit 11:13

--- __init__.py
+++ __init__.py
@@ -1,1 +1,3 @@
-from . import  lolModelo
+# -*- coding: utf-8 -*-
+
+from . import modelos
--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-	 'name': "lol",
-    'summary': """league of legends""",
+	'name': "examen",
+    'summary': """examen odoo""",
     'description': """
-        Lol module to record matches score:
+        examen SGE 1Ev Odoo
     """,
-    'author': "unknown",
-    'website': "http://www.salesuanos.edu",
+    'author': "Jon",
+    'website': "http://www.salesianos.edu",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'LOL',
+    'category': 'odoo',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
